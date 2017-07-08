17740ffaf2e1a7564029fc8ffb07359488118ca4
Jose Roberts <webterpr@gmail.com>
2017-07-08 13:52:16 -0400

    added routes dir and index.js

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var routes = require('./routes');
+
 app.set('port', 3000);
 
+app.use(function(req, res, next){
+    console.log(req.method, req.url);
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', routes);
 
 // app.get('/', function(req, res){
 //     console.log("GET the hompage");
