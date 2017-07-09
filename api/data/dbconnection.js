1f6be80e03a7f2f15b87c01b436c19dc9db8befe
Jose Roberts <webterpr@gmail.com>
2017-07-08 20:23:38 -0400

    created db connection calls

---
+++ api/data/dbconnection.js
@@ -0,0 +1,24 @@
+var MongoClient = require('mongodb').MongoClient;
+var dburl = 'mongodb://localhost:27017/meanhotel';
+
+var _connection = null;
+var open = function(){
+    MongoClient.connect(dburl, function(err, db){
+        if(err){
+            console.log("connection failed");
+            return;
+        }
+        _connection = db;
+        console.log("DB connected", db);
+    });
+    //set conn
+};
+
+var get = function(){
+    return _connection;
+};
+
+module.exports = {
+    open: open,
+    get: get
+};
