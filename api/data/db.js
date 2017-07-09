21d4c45d93398398a631eb430aeb866bcbebac61
Jose Roberts <webterpr@gmail.com>
2017-07-08 22:28:17 -0400

    created mongoose connections

--- api/data/db.js
+++ api/data/db.js
@@ -8,9 +8,30 @@ mongoose.connection.on('connected', function(){
 });
 
 mongoose.connection.on('disconnected', function(){
-    console.log('disconnect mongoose ' + dburl);
+    console.log('disconnect mongoose ');
 });
 
 mongoose.connection.on('error', function(err){
-    console.log('mongoose error ' + dburl);
+    console.log('mongoose error ' + err);
+});
+
+process.on('SIGINT', function(){
+    mongoose.connection.close(function(){
+        console.log('Mongoose disconnected through app termination (SIGINT)');
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function(){
+    mongoose.connection.close(function(){
+        console.log('Mongoose disconnected through app termination (SIGTERM)');
+        process.exit(0);
+    });
+});
+
+process.once('SIGUSR2', function(){
+    mongoose.connection.close(function(){
+        console.log('Mongoose disconnected through app termination (SIGUSR2)');
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
