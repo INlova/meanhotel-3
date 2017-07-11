3367e1fa5facee355609c246523e6d72516b8f81
Jose Roberts <webterpr@gmail.com>
2017-07-11 00:48:54 -0400

    backend setup, moving to front end

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -129,29 +129,143 @@ module.exports.hotelsGetOne = function(req, res){
     //     .json(doc);
     // });
 };
+var _splitArray = function(input){
+    var output;
+    if (input && input.length > 0){
+        output = input.split(";");
+    }else{
+        output = [];
+    }
+    return output;
+}
 
 module.exports.hotelsAddOne = function(req, res){
-    var db = dbconn.get();
-    var collection = db.collection('hotels');
-    var newHotel;
-    
-    console.log("New hotel");
 
-    if(req.body && req.body.name && req.body.stars){
-        newHotel = req.body;
-        newHotel.stars = parseInt(req.body.stars, 10);
-        collection.insertOne(newHotel, function(err, response){
-            console.log(response);
-            console.log(response.ops);
+    Hotel
+    .create({
+        name : req.body.name,
+        description : req.body.description,
+        stars : parseInt(req.body.stars, 10),
+        services : _splitArray(req.body.services),
+        photos : _splitArray(req.body.photos),
+        currency : req.body.currency,
+        location : {
+            address : req.body.address,
+            coordinates : [
+                parseFloat(req.body.lng),
+                parseFloat(req.body.lat)
+            ]
+        }
+    }, function(err, hotel){
+        if(err){
+            console.log('error creating hotel');
             res
-                .status(201)
-                .json(response.ops);
-        });
-                } else {
-                    console.log('data is missisng');
-                    res
-                        .status(400)
-                        .json({message : "reqired data"});
-                }   
+            .status(400)
+            .json(err);
+        }else{
+            console.log('created hotel' ,hotel);
+            res
+            .status(201)
+            .json(hotel);
+        }
+    });
+    // var db = dbconn.get();
+    // var collection = db.collection('hotels');
+    // var newHotel;
     
+    // console.log("New hotel");
+
+    // if(req.body && req.body.name && req.body.stars){
+    //     newHotel = req.body;
+    //     newHotel.stars = parseInt(req.body.stars, 10);
+    //     collection.insertOne(newHotel, function(err, response){
+    //         console.log(response);
+    //         console.log(response.ops);
+    //         res
+    //             .status(201)
+    //             .json(response.ops);
+    //     });
+    //             } else {
+    //                 console.log('data is missisng');
+    //                 res
+    //                     .status(400)
+    //                     .json({message : "reqired data"});
+    //             }   
+    
+};
+
+module.exports.hotelsUpdateOne = function(req, res){
+    var hotelId = req.params.hotelId;
+    console.log('for hotel id' , hotelId);
+    
+    Hotel
+    .findById(hotelId)
+    .select("-reviews -rooms")
+    .exec(function(err, doc){
+        var response = {
+            satus : 200,
+            message : doc
+        };
+        if(err){
+            console.log("error finding hotel");
+            res
+            .status(500)
+            .json(err);
+        }else if(!doc){
+            console.log("hotel id " + hotelId + " not found");
+            res
+            .status(404)
+            .json({"message": "trouble finding the is"});
+        }else{
+            console.log("ready to update hotel id",hotelId);
+
+            doc.name = req.body.name;
+            doc.description = req.body.description;
+            doc.stars = parseInt(req.body.stars, 10);
+            doc.services = _splitArray(req.body.services);
+            doc.photos = _splitArray(req.body.photos);
+            doc.currency = req.body.currency;
+            doc.location = {
+                address: req.body.address,
+                coordinates : [
+                    parseFloat(req.body.lng),
+                    parseFloat(req.body.lat)
+            ]
+        };
+        doc.save(function(err, hotelsUpdated){
+            if(err){
+                console.log("error updating hotel");
+                res
+                .status(404)
+                .json(err);
+            }else{
+                console.log("hotel updated");
+                res
+                .status(204)
+                .json();
+            }
+        })
+        };  
+    });
+};
+
+module.exports.hotelsDeleteOne = function(req, res){
+    var hotelId = req.params.hotelId;
+
+    Hotel
+    .findByIdAndRemove(hotelId)
+    .exec(function(err, hotel){
+        if (err){
+            res
+            .status(404)
+            .json(err);
+        }else{
+           console.log("hotel id " + hotelId + " deleted");
+           res
+           .status(204)
+           .json();
+        };
+
+    });
+
 };
--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -10,9 +10,21 @@ module.exports.reviewsGetAll = function(req, res){
     .findById(hotelId)
     .select('reviews')
     .exec(function(err, doc){
-        res
-        .status(200)
-        .json(doc.reviews);
+       if (err){
+           console.log('error finding hotel');
+           res
+           .status(500)
+           .json(err);
+       }else if (!doc){
+           console.log('hot id not found in db', hotelId);
+           res
+           .status(404)
+           .json({'messgae': 'hotel id ' + hotelId + ' not found'});
+       }else{
+           res
+           .status(200)
+           .json(doc.reviews ? doc.reviews : []);
+       } 
     });
 };
 
@@ -25,10 +37,182 @@ module.exports.reviewsGetOne = function(req, res){
     .findById(hotelId)
     .select('reviews')
     .exec(function(err, hotel){
+        if(err){
+            console.log('error finding hotel');
+            res
+            .status(500)
+            .json(err);
+        }else if(!hotel){
+            console.log("no hotel with that id " + hotelId + " found");
+            res
+            .status(400)
+            .json({"message":"hotel with id " + hotelId + " not found"})
+        }else if(!hotel.reviews.id(reviewId)){
+            console.log("review id " + reviewId + " not found");
+            res
+            .status(400)
+            .json({"message":"no review id " + reviewId + " for hotel id " + hotelId });
+
+        }else{
         console.log("returned hotel", hotel);
         var review = hotel.reviews.id(reviewId);
         res
         .status(200)
         .json(review);
+        };
     });
+};
+
+var _addReview = function(req, res, hotel){
+    hotel.reviews.push({
+        name: req.body.name,
+        rating: parseInt(req.body.rating, 10),
+        review: req.body.review
+    });
+    hotel.save(function(err, hotelUpdated){
+        if (err){
+            res
+            .status(500)
+            .json(err);
+        }else{
+            res
+            .status(201)
+            .json(hotelUpdated.reviews[hotelUpdated.reviews.length - 1]);
+        }
+    });
+};
+
+module.exports.reviewsAddOne = function(req, res){
+    var hotelId = req.params.hotelId;
+    console.log("Get hotel id ", hotelId);
+
+    Hotel
+    .findById(hotelId)
+    .select('reviews')
+    .exec(function(err, doc){
+       
+       if (err){
+           console.log('error finding hotel');
+           res
+           .status(500)
+           .json(err);
+       }else if (!doc){
+           console.log('hot id not found in db', hotelId);
+           res
+           .status(404)
+           .json({'messgae': 'hotel id ' + hotelId + ' not found'});
+       }
+       if(doc){
+           _addReview(req, res, doc);
+       }else{
+           res
+           .status(200)
+           .json(doc.reviews ? doc.reviews : []);
+       }       
+    });
+};
+
+module.exports.reviewsUpdateOne = function(req, res){
+    var hotelId = req.params.hotelId;
+    var reviewId = req.params.reviewId;
+    console.log("Get review id " + reviewId + " for hotel id " + hotelId);
+
+    Hotel
+    .findById(hotelId)
+    .select('reviews')
+    .exec(function(err, hotel){
+        if(err){
+            console.log('error finding hotel');
+            res
+            .status(500)
+            .json(err);
+        }else if(!hotel){
+            console.log("no hotel with that id " + hotelId + " found");
+            res
+            .status(400)
+            .json({"message":"hotel with id " + hotelId + " not found"})
+        }else if(!hotel.reviews.id(reviewId)){
+            console.log("review id " + reviewId + " not found");
+            res
+            .status(404)
+            .json({"message":"no review id " + reviewId + " for hotel id " + hotelId });
+
+        }else{
+        console.log("updating hotel review", hotel);
+        var review = hotel.reviews.id(reviewId);
+        if (!review){
+            res
+            .status(404)
+            .json({"message" : "review id " + reviewId + " not found" });
+        }else{
+            review.name = req.body.name;
+            review.rating = parseInt(req.body.rating, 10);
+            review.review = req.body.review;
+            hotel.save(function(err,hotelUpdated){
+                if (err){
+                    res
+                    .status(500)
+                    .json(err);
+                }else{
+                    
+                    res
+                    .status(200)
+                    .json();
+                }
+            });
+        };
+    };
+
+});
+};
+
+module.exports.reviewsDeleteOne = function(req, res){
+var hotelId = req.params.hotelId;
+    var reviewId = req.params.reviewId;
+    console.log("Get review id " + reviewId + " for hotel id " + hotelId);
+
+    Hotel
+    .findById(hotelId)
+    .select('reviews')
+    .exec(function(err, hotel){
+        if(err){
+            console.log('error finding hotel');
+            res
+            .status(500)
+            .json(err);
+        }else if(!hotel){
+            console.log("no hotel with that id " + hotelId + " found");
+            res
+            .status(400)
+            .json({"message":"hotel with id " + hotelId + " not found"})
+        }else if(!hotel.reviews.id(reviewId)){
+            console.log("review id " + reviewId + " not found");
+            res
+            .status(404)
+            .json({"message":"no review id " + reviewId + " for hotel id " + hotelId });
+
+        }else{
+        console.log("updating hotel review", hotel);
+        var review = hotel.reviews.id(reviewId);
+        if (!review){
+            res
+            .status(404)
+            .json({"message" : "review id " + reviewId + " not found" });
+        }else{
+           hotel.reviews.id(reviewId).remove();
+            hotel.save(function(err,hotelUpdated){
+                if (err){
+                    res
+                    .status(500)
+                    .json(err);
+                }else{
+                    res
+                    .status(204)
+                    .json();
+                }
+            });
+        };
+    };
+
+});
 };
