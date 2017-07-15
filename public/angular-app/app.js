5758454d31295cff0fe2d0e6198c2074a69f4a85
Jose Roberts <webterpr@gmail.com>
2017-07-15 19:47:57 -0400

    working on displaying additional data

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -1,17 +1,21 @@
-angular.module('meanhotel', ['ngRoute'])
-.config(config)
-.config('HotelsController', HotelsController);
-
-function config($routeProvider){
+angular.module('meanhotel', ['ngRoute']).config(config);
+// .config('HotelsController', HotelsController);
+function config($routeProvider,$locationProvider) {
+    $locationProvider.hashPrefix('');
     $routeProvider
     .when('/', {
-        templateUrl: 'angular-app/hotels.html',
+        templateUrl: 'angular-app/hotel-list/hotel.html',
         controller: HotelsController,
+        controllerAs: 'vm'
+    })
+    .when('/hotel/:id', {
+        templateUrl: 'angular-app/hotel-display/hotel.html',
+        controller: HotelController,
         controllerAs: 'vm'
     });
 }
 
-function HotelsController(){
-    var vm = this;
-    vm.title = 'MEAN Hotel App';
-}
+// function HotelsController(){
+//     var vm = this;
+//     vm.title = 'MEAN Hotel App';
+// }
---
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -0,0 +1,11 @@
+angular.module('meanhotel').controller('HotelsController', HotelsController);
+
+function HotelsController(hotelDataFactory){
+    var vm = this;
+    vm.title = 'MEAN Hotel App';
+    //     $http.get('/api/hotels?count=10').then(function(response){
+    hotelDataFactory.hotelList().then(function(response){
+        console.log(response);
+        vm.hotels = response;
+    });
+}
