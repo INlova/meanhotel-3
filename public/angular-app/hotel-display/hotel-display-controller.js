2d4071967978aaa8a92ccf3840d9f364cf22d7da
Jose Roberts <webterpr@gmail.com>
2017-07-15 22:53:11 -0400

    added submit form for review

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -1,13 +1,33 @@
 angular.module('meanhotel').controller('HotelController', HotelController);
 
-function HotelController(hotelDataFactory, $routeParams){
-    var vm = this;
-    var id = $routeParams.id;
-    hotelDataFactory.hotelDisplay(id).then(function(response){
-        vm.hotel = response;
-        vm.stars = _getStarRating(response.stars);
-    });
-    function _getStarRating(stars){
-        return new Array(stars);
+function HotelController($route, $routeParams, hotelDataFactory) {
+  var vm = this;
+  var id = $routeParams.id;
+  vm.isSubmitted = false;
+  hotelDataFactory.hotelDisplay(id).then(function(response) {
+    vm.hotel = response;
+    vm.stars = _getStarRating(response.stars);
+  });
+
+  function _getStarRating(stars) {
+    return new Array(stars);
+  }
+  vm.addReview = function() {
+    var postData = {
+      name: vm.name,
+      rating: vm.rating,
+      review: vm.review
+    };
+    if (vm.reviewForm.$valid) {
+      hotelDataFactory.postReview(id, postData).then(function(response) {
+        if (response.status === 200) {
+          $route.reload();
+        }
+      }).catch(function(error) {
+        console.log(error);
+      });
+    } else {
+      vm.isSubmitted = true;
     }
+  };
 }
